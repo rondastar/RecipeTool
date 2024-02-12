071238c128293e56ff3ad149395b3d0c3eee3a94
rondastar <116147793+rondastar@users.noreply.github.com>
2024-02-11 18:30:04 -0800

    Added Unit

---
+++ Unit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeTool
+{
+    internal class Unit
+    {
+        private string _name;
+
+        public Unit(string name)
+        {
+            _name = name;
+        }
+
+        public string Name { get => _name; set => _name = value; }
+    }
+}
