455223e0ec07156b609ea7051122d9b10c4e9545
rondastar <116147793+rondastar@users.noreply.github.com>
2024-02-11 18:32:26 -0800

    Added IngredientLine class

---
+++ IngredientLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeTool
+{
+    internal class IngredientLine
+    {
+        private Quantity _qty;
+        private Unit _unit;
+        private Ingredient _ingredient;
+
+        public IngredientLine(Quantity qty, Unit unit, Ingredient ingredient)
+        {
+            _qty = qty;
+            _unit = unit;
+            _ingredient = ingredient;
+        }
+
+        public IngredientLine(Quantity qty, Ingredient ingredient)
+        {
+            _qty = qty;
+            _ingredient = ingredient;
+        }
+
+        public IngredientLine(Ingredient ingredient) { _ingredient = ingredient; }
+    }
+}
