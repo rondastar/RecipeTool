73b55470ec018b1edbe4cd15bbac0672fed009bf
rondastar <116147793+rondastar@users.noreply.github.com>
2024-02-11 18:14:11 -0800

    Added Ingredient class

---
+++ Ingredient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeTool
+{
+    internal class Ingredient
+    {
+        private string name;
+
+        public Ingredient(string name)
+        {
+            this.name = name;
+        }
+
+        internal string Name { get => name; set => name = value; }
+    }
+}
+
